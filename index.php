477b3262fd47973b3a8794cbdc336a5b25061835
sharifloo-net <sharifloo.net@gmail.com>
2024-05-15 14:18:54 +0330

    init project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>کوتاه کننده لینک</title>
+    <link rel="stylesheet" href="themes/default/css/style.css">
+</head>
+<body>
+<form action="" method="post">
+    <input type="text" name="url" placeholder="لینک را وارد کنید">
+    <input type="submit" value="کوتاه کن" name="shorten">
+</form>
+</body>
+</html>
