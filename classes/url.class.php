ca6067944ad1377f4eed1e65beccd7f99970d1d3
sharifloo-net <sharifloo.net@gmail.com>
2024-05-15 15:28:29 +0330

    add url.class.php

---
+++ classes/url.class.php
@@ -0,0 +1,24 @@
+<?php
+class url extends dbh
+{
+    protected function setUrl($originalUrl, $shortenedUrl)
+    {
+        $sql = 'INSERT INTO url (originalUrl, shortenedUrl) VALUES (?, ?)';
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$originalUrl, $shortenedUrl]);
+    }
+    protected function checkUrl($originalUrl)
+    {
+        $sql = 'SELECT originalUrl FROM url WHERE originalUrl = ?';
+        $stmt = $this->connect()->prepare($sql);
+        if (!$stmt->execute([$originalUrl]))
+        {
+            $stmt = null;
+            header('location: ../?error=stmtFailed');
+            exit;
+        }
+        $urlData = $stmt->fetchAll();
+        if ($stmt->rowCount() > 0) return $urlData[0]['shortenedUrl'];
+        return false;
+    }
+}
