641695572d4a32cd555540fd10ff437b16ca8262
sharifloo-net <sharifloo.net@gmail.com>
2024-05-15 16:24:39 +0330

    require_once required files

--- includes/url.inc.php
+++ includes/url.inc.php
@@ -1,8 +1,14 @@
 <?php
 require_once 'functions.inc.php';
+
 if (post('shorten')) {
     $originalUrl = post('url');
     $shortenedUrl = $rand = substr(md5(microtime()), rand(0, 26), 5);
+
+    require_once '../classes/dbh.class.php';
+    require_once '../classes/url.class.php';
+    require_once '../classes/urlContr.class.php';
+
     $url = new urlContr($originalUrl, $shortenedUrl);
     $url->setUrl();
     header("location: ../?shortenedUrl=http://localhost/$shortenedUrl");
