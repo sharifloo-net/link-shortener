219dd8b55c40572061afc7d2afc9bcfbdc4e77e0
sharifloo-net <sharifloo.net@gmail.com>
2024-05-19 14:15:09 +0330

    Add admin panel, header|footer.php

---
+++ admin/index.php
@@ -0,0 +1,8 @@
+<?php require_once 'header.php' ?>
+    <h1>Welcome dear admin :)</h1>
+    [<a href="custom.php">Add a custom shortened link</a>]
+    <br><br>
+    [<a href="random.php">Add a random shortened link</a>]
+    <br><br>
+    [<a href="logout.php">Logout</a>]
+<?php require_once 'footer.php' ?>
