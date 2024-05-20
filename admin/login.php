8364688490b206323932b6061a8dfe09b4137e37
sharifloo-net <sharifloo.net@gmail.com>
2024-05-20 15:23:08 +0330

    Add redirect to index.php when admin is logged in

--- admin/login.php
+++ admin/login.php
@@ -1,4 +1,7 @@
-<?php require_once 'header.php' ?>
+<?php
+require_once 'main.php';
+require_once 'header.php'
+?>
     <h1>Admin panel login page</h1>
     <form action="../includes/login.inc.php" method="post">
         <input type="text" name="username" placeholder="username"><br><br>
--- admin/main.php
+++ admin/main.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
+$isInLogin = str_contains($_SERVER['REQUEST_URI'], 'login.php');
 if (isset($_SESSION['userPass']) || isset($_COOKIE['userPass'])) {
     require_once '../classes/config.class.php';
     require_once '../classes/auth.class.php';
     $userPass = $_SESSION['userPass'] ?? $_COOKIE['userPass'];
     $auth = new auth($userPass);
-} else {
+    if ($isInLogin) {
+        header('location: ./');
+        exit;
+    }
+} else if (!$isInLogin) {
     header('location: login.php');
     exit;
 }
