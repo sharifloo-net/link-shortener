88577149ef489ace84b5aa2eca63f6f1eefb165b
sharifloo-net <sharifloo.net@gmail.com>
2024-05-24 16:34:02 +0330

    Add autofocus input

--- themes/default/js/script.js
+++ themes/default/js/script.js
@@ -1,11 +1,16 @@
+const input = document.querySelector('#input');
+
+input.focus();
+
 document.querySelector('#btn').onclick = () => {
-    if (!document.querySelector('#input').value) {
+    if (input.value) {
         Swal.fire({
             title: 'فیلد خالی است!',
             text: 'لطفا یک لینک وارد کنید',
             icon: 'warning',
             confirmButtonText: 'باشه'
         });
+
         // sweetalert2 config. (for keeping body height 100%)
         document.body.classList.remove('swal2-height-auto');
         return false;
