4813d0b4b634a4786e8998d822ce3743d35f2fe1
sharifloo-net <sharifloo.net@gmail.com>
2024-06-26 17:23:24 +0330

    Add semi-colon :\

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'main.php';
-require_once 'header.php'
+require_once 'header.php';
 ?>
     <h1>Welcome dear admin :)</h1>
     [<a href="customUrl.php">Add a custom shortened link</a>]
