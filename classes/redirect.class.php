61e9dc3dd5b1b3f340424d55dad6baeb457f0601
sharifloo-net <sharifloo.net@gmail.com>
2024-07-27 15:51:31 +0330

    Add 404 page for shortened links

--- classes/redirect.class.php
+++ classes/redirect.class.php
@@ -1,27 +1,30 @@
 <?php
+
 class redirect extends dbh
 {
     private string $shortenedUrl;
+
     public function __construct($shortenedUrl)
     {
         $this->shortenedUrl = $shortenedUrl;
     }
+
     public function getOriginalUrl()
     {
-            $sql = 'SELECT originalUrl FROM url WHERE shortenedUrl = ?';
-            $stmt = $this->connect()->prepare($sql);
-            if (!$stmt->execute([$this->shortenedUrl]))
-            {
-                $stmt = null;
-                header('location: ../?error=stmtFailed');
-                exit;
-            }
-            if ($stmt->rowCount() > 0) {
-                $urlData = $stmt->fetchAll();
-                $originalUrl = $urlData[0]['originalUrl'];
-                header("location: $originalUrl");
-                exit;
-            }
-            echo 'Short URL not found!';
+        $sql = 'SELECT originalUrl FROM url WHERE shortenedUrl = ?';
+        $stmt = $this->connect()->prepare($sql);
+        if (!$stmt->execute([$this->shortenedUrl])) {
+            $stmt = null;
+            header('location: ../?error=stmtFailed');
+            exit;
+        }
+        if ($stmt->rowCount() > 0) {
+            $urlData = $stmt->fetchAll();
+            $originalUrl = $urlData[0]['originalUrl'];
+            header("location: $originalUrl");
+            exit;
+        }
+        session_start();
+        $_SESSION['notFound'] = 1;
     }
 }
